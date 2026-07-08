07871b21f12cdbab98d6ff18031a48d5c006e772
yueyinqiu <yueyinqiu@outlook.com>
2026-07-08 13:42:04 +0800

    revert

--- earth-latitude-7490/yueyinqiu/fcitx5/default.nix
+++ earth-latitude-7490/yueyinqiu/fcitx5/default.nix
@@ -1,20 +1,14 @@
-{ pkgs, ... }: {
-    i18n.inputMethod = {
-        enable = true;
-        type = "fcitx5";
-        fcitx5 = {
-            addons = with pkgs; [ qt6Packages.fcitx5-chinese-addons fcitx5-rime ];
-            settings.inputMethod = {
-                GroupOrder."0" = "Default";
-                "Groups/0" = {
-                    Name = "Default";
-                    "Default Layout" = "cn-altgr-pinyin";
-                    DefaultIM = "keyboard-us";
-                };
-                "Groups/0/Items/0".Name = "pinyin";
-                "Groups/0/Items/1".Name = "keyboard-us";
-            };
-            ignoreUserConfig = true;
+{ ... }: {
+    i18n.inputMethod.fcitx5.settings.inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "cn-altgr-pinyin";
+            DefaultIM = "keyboard-us";
         };
+        "Groups/0/Items/0".Name = "pinyin";
+        "Groups/0/Items/1".Name = "keyboard-us";
     };
+    # i18n.inputMethod.fcitx5.settings.globalOptions = { };
+    i18n.inputMethod.fcitx5.ignoreUserConfig = true;
 }
