118b06e9d7e776caabe38154f014f9281017d203
yueyinqiu <yueyinqiu@outlook.com>
2026-07-08 23:32:19 +0800

    spawn on startup?

--- earth-latitude-7490/yueyinqiu/niri/default.nix
+++ earth-latitude-7490/yueyinqiu/niri/default.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
     xdg.configFile."niri/config-binds.kdl".source = ./config-binds.kdl;
+    xdg.configFile."niri/config-spawn-at-startup.kdl".source = ./config-spawn-at-startup.kdl;
     xdg.configFile."niri/config.kdl".source = ./config.kdl;
 }
