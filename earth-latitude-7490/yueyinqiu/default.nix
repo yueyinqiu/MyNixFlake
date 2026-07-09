ce9aac2394f41933d04224ce6e9823fdfdda8758
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 00:07:09 +0800

    why

--- earth-latitude-7490/yueyinqiu/default.nix
+++ earth-latitude-7490/yueyinqiu/default.nix
@@ -1,18 +1,16 @@
-{ pkgs, lib, chinese-fonts-overlay, ... }: {
+{ pkgs, lib, ... }: {
     home.username = "yueyinqiu";
     home.homeDirectory = "/home/yueyinqiu";
 
     programs.home-manager.enable = true;
 
     nixpkgs.config.allowUnfree = true;
-    nixpkgs.overlays = [ chinese-fonts-overlay.overlays.default ];
 
     home.packages = with pkgs; [
         noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
         noto-fonts-color-emoji
-        microsoftPackages.simsun
     ];
 
     fonts.fontconfig = {
