cd7e61c179b22d20474d1d4148d50acef60f2350
yueyinqiu <yueyinqiu@outlook.com>
2026-07-09 17:22:37 +0800

    s

--- earth-latitude-7490/yueyinqiu/niri/spawn-at-startup.sh
+++ earth-latitude-7490/yueyinqiu/niri/spawn-at-startup.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-niri msg action focus-workspace 1
-
-niri msg action spawn "FlClash"
+niri msg action spawn -- FlClash
+niri msg action spawn -- qq
+niri msg action spawn -- run wechat
 
 niri msg action focus-workspace 2
-niri msg action spawn "qq"
-niri msg action spawn-sh "run wechat"
-
-niri msg action focus-workspace 3
