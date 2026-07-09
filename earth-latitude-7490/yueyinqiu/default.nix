fefc9345675fb921b7d2811d882fc80f6a1b71f8
yueyinqiu <yueyinqiu@outlook.com>
2026-07-09 19:54:35 +0800

    try font

--- earth-latitude-7490/yueyinqiu/default.nix
+++ earth-latitude-7490/yueyinqiu/default.nix
@@ -7,17 +7,21 @@
     nixpkgs.config.allowUnfree = true;
 
     home.packages = with pkgs; [
+        noto-fonts
         noto-fonts-cjk-sans
         noto-fonts-cjk-serif
         noto-fonts-color-emoji
+        noto-fonts-monospace
     ];
 
     fonts.fontconfig.enable = true;
 
     fonts.fontconfig.defaultFonts = {
-        serif = [ "Noto Serif CJK SC" ];
-        sansSerif = [ "Noto Sans CJK SC" ];
+        serif = [ "Noto Serif" "Noto Serif CJK SC" ];
+        sansSerif = [ "Noto Sans" "Noto Sans CJK SC" ];
+        monospace = [ "Noto Sans Mono" "Noto Sans CJK SC" ];
     };
+
     imports = [ ./packages ];
 
     home.stateVersion = "26.05";
