6172719d90d154ff0a8856be18be6a3c77fa0a49
yueyinqiu <yueyinqiu@outlook.com>
2026-07-09 20:37:43 +0800

    ff

--- earth-latitude-7490/yueyinqiu/packages/fcitx5/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/fcitx5/default.nix
@@ -3,24 +3,29 @@
         enable = true;
         type = "fcitx5";
         fcitx5.addons = with pkgs; [ qt6Packages.fcitx5-chinese-addons ];
-        # fcitx5.settings.inputMethod = {
-        #     GroupOrder."0" = "Default";
-        #     "Groups/0" = {
-        #         Name = "Default";
-        #         "Default Layout" = "cn-altgr-pinyin";
-        #         DefaultIM = "keyboard-us";
-        #     };
-        #     "Groups/0/Items/0".Name = "pinyin";
-        #     "Groups/0/Items/1".Name = "keyboard-us";
-        # };
-        # fcitx5.settings.addons = {
-        #     cloudpinyin = {
-        #         globalSection = {
-        #             Backend = "Baidu";
-        #             MinimumPinyinLength = "2";
-        #         };
-        #     };
-        # };
-        # fcitx5.ignoreUserConfig = true;
+        fcitx5.settings.inputMethod = {
+            GroupOrder."0" = "Default";
+            "Groups/0" = {
+                Name = "Default";
+                "Default Layout" = "cn-altgr-pinyin";
+                DefaultIM = "keyboard-us";
+            };
+            "Groups/0/Items/0".Name = "pinyin";
+            "Groups/0/Items/1".Name = "keyboard-us";
+        };
+        fcitx5.settings.addons = {
+            cloudpinyin = {
+                globalSection = {
+                    Backend = "Baidu";
+                    MinimumPinyinLength = "2";
+                };
+            };
+            pinyin = {
+                globalSection = {
+                    CloudPinyinEnabled = true;
+                };
+            };
+        };
+        fcitx5.ignoreUserConfig = true;
     };
 }
