094db8fe66c4a70bbc93ebab3d8141c105780f6e
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 19:23:40 +0800

    mdt

--- earth-latitude-7490/yueyinqiu/packages/mindustry/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/mindustry/default.nix
@@ -4,8 +4,8 @@
     ];
     my.r.mindustry = "mindustry";
     
-    # services.flatpak.packages = [
-    #     "flathub:app/com.github.Anuken.Mindustry/x86_64/stable"
-    # ];
-    # my.r.mindustry-flatpak = "flatpak run com.github.Anuken.Mindustry";
+    services.flatpak.packages = [
+        "flathub:app/com.github.Anuken.Mindustry/x86_64/stable"
+    ];
+    my.r.mindustry-flatpak = "flatpak run com.github.Anuken.Mindustry";
 }
