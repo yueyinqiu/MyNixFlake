bbe284a17f97dba9d0b0e8a0641b99485d821671
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 17:53:12 +0800

    change remote ...

--- earth-latitude-7490/yueyinqiu/packages/flatpak/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/flatpak/default.nix
@@ -6,7 +6,7 @@
     services.flatpak = {
         enable = true;
         remotes = {
-            "flathub" = "https://mirror.sjtu.edu.cn/flathub";
+            "flathub" = "https://dl.flathub.org/repo";
         };
     };
 }
