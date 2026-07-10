c0bd57105fd53d2544fc17a660d7537d9a8d6865
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 09:17:48 +0800

    flatpak meet

--- earth-latitude-7490/yueyinqiu/packages/wemeet/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wemeet/default.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
-    home.packages = with pkgs; [
-        wemeet
+    services.flatpak.packages = [
+        "com.tencent.wemeet"
     ];
+    my.r.wemeet = "flatpak run com.tencent.wemeet";
 }
