e926e65303597586c882dc768058782670625b9e
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 16:45:44 +0800

    com2org

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
     description = "yueyinqiu's flake";
 
     inputs = {
-        nixpkgs-proxy.url = "git+https://gh-proxy.com/https://github.com/NixOS/nixpkgs.git?ref=nixos-unstable&shallow=1";
+        nixpkgs-proxy.url = "git+https://gh-proxy.org/https://github.com/NixOS/nixpkgs.git?ref=nixos-unstable&shallow=1";
         
-        home-manager-proxy.url = "git+https://gh-proxy.com/https://github.com/nix-community/home-manager.git?ref=master&shallow=1";
+        home-manager-proxy.url = "git+https://gh-proxy.org/https://github.com/nix-community/home-manager.git?ref=master&shallow=1";
         home-manager-proxy.inputs.nixpkgs.follows = "nixpkgs-proxy";
 
-        flatpaks-proxy.url = "git+https://gh-proxy.com/https://github.com/in-a-dil-emma/declarative-flatpak.git?ref=latest&shallow=1";
+        flatpaks-proxy.url = "git+https://gh-proxy.org/https://github.com/in-a-dil-emma/declarative-flatpak.git?ref=latest&shallow=1";
 
-        flake-compat-proxy.url = "git+https://gh-proxy.com/https://github.com/edolstra/flake-compat.git?ref=master&shallow=1";
-        nix-filter-proxy.url = "git+https://gh-proxy.com/https://github.com/numtide/nix-filter.git?ref=main&shallow=1";
-        flake-utils-proxy.url = "git+https://gh-proxy.com/https://github.com/numtide/flake-utils.git?ref=main&shallow=1";
+        flake-compat-proxy.url = "git+https://gh-proxy.org/https://github.com/edolstra/flake-compat.git?ref=master&shallow=1";
+        nix-filter-proxy.url = "git+https://gh-proxy.org/https://github.com/numtide/nix-filter.git?ref=main&shallow=1";
+        flake-utils-proxy.url = "git+https://gh-proxy.org/https://github.com/numtide/flake-utils.git?ref=main&shallow=1";
 
-        winapps-proxy.url = "git+https://gh-proxy.com/https://github.com/winapps-org/winapps.git?ref=main&shallow=1";
+        winapps-proxy.url = "git+https://gh-proxy.org/https://github.com/winapps-org/winapps.git?ref=main&shallow=1";
         winapps-proxy.inputs.nixpkgs.follows = "nixpkgs-proxy";
         winapps-proxy.inputs.nix-filter.follows = "nix-filter-proxy";
         winapps-proxy.inputs.flake-utils.follows = "flake-utils-proxy";
