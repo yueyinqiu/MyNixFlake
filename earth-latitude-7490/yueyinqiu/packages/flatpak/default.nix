14fb92810bb29eebfcebc508bb9c4b4994d30239
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 18:57:56 +0800

    mirror

--- earth-latitude-7490/yueyinqiu/packages/flatpak/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/flatpak/default.nix
@@ -8,5 +8,8 @@
         remotes = {
             "flathub" = "https://mirror.sjtu.edu.cn/flathub/flathub.flatpakrepo";
         };
+        preInstallCommand = ''
+            flatpak --user remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
+        '';
     };
 }
