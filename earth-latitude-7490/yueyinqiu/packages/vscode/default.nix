b78b0a661f95055e54ac54e2d1b09799673d63f2
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 10:32:26 +0800

    os note

--- earth-latitude-7490/yueyinqiu/packages/vscode/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/vscode/default.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
     programs.vscode.enable = true;
     my.r.vscode = "code";
+    my.r.os = "code ~/MyProjects/Nix/MyNixFlake";
+    my.r.note = "code ~/MyMiscellaneousFiles/Notes";
 }
