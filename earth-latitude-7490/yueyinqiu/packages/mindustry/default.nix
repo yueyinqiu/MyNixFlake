2ac2a11dba0f1b31386ba7af8aa7b18962a11e3b
yueyinqiu <yueyinqiu@outlook.com>
2026-07-10 16:01:10 +0800

    remove flatpak

--- earth-latitude-7490/yueyinqiu/packages/mindustry/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/mindustry/default.nix
@@ -2,9 +2,10 @@
     home.packages = with pkgs; [
         mindustry
     ];
-    services.flatpak.packages = [
-        "com.github.Anuken.Mindustry"
-    ];
     my.r.mindustry = "mindustry";
-    my.r.mindustry-flatpak = "flatpak run com.github.Anuken.Mindustry";
+    
+    # services.flatpak.packages = [
+    #     "com.github.Anuken.Mindustry"
+    # ];
+    # my.r.mindustry-flatpak = "flatpak run com.github.Anuken.Mindustry";
 }
