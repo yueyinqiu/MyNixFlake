5b0ce8bf06d1cd1feadd0840e14e3cd803e9c7db
yueyinqiu <yueyinqiu@outlook.com>
2026-07-11 22:31:11 +0800

    win10

--- earth-latitude-7490/yueyinqiu/packages/libvirt/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/libvirt/default.nix
@@ -33,5 +33,20 @@
                 install_virtio = true;
             });
         }
+        {
+            definition = nixvirt.lib.domain.writeXML (nixvirt.lib.domain.templates.windows
+            {
+                name = "win10";
+                uuid = "90d049a8-dca2-472e-8212-3019254df766";
+                memory = { count = 4; unit = "GiB"; };
+                # qemu-img create -f qcow2 /home/yueyinqiu/VirtualMachines/win10/storage.qcow2 128G
+                storage_vol = /home/yueyinqiu/VirtualMachines/win10/storage.qcow2;
+                install_vol = /home/yueyinqiu/VirtualMachines/win10/install.iso;
+                nvram_path = /home/yueyinqiu/VirtualMachines/win10/nvram.nvram;
+                virtio_net = true;
+                virtio_drive = true;
+                install_virtio = true;
+            });
+        }
     ];
 }
