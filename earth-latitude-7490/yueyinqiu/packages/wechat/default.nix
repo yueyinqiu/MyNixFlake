dac4945bb031eb428cc077d5f66d96e3777e6fd9
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 00:25:47 +0800

    wechat fix

--- earth-latitude-7490/yueyinqiu/packages/wechat/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wechat/default.nix
@@ -2,5 +2,6 @@
     home.packages = with pkgs; [
         wechat
     ];
-    my.r.wechat = "wechat";
+    # https://zhuanlan.zhihu.com/p/2051087997395808628
+    my.r.wechat = "env WAYLAND_DISPLAY= DISPLAY=:0 QT_QPA_PLATFORM=xcb GTK_IM_MODULE=fcitx QT_IM_MODULE=fcitx XMODIFIERS=@im=fcitx wechat";
 }
