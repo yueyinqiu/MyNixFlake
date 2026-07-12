3eb08f3801bde0124bbe61711ddf3d9a2dd66092
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 20:05:30 +0800

    x

--- os-proxy.sh
+++ os-proxy.sh
@@ -5,6 +5,9 @@ Environment="https_proxy=http://localhost:52207"
 Environment="http_proxy=http://localhost:52207"
 Environment="all_proxy=http://localhost:52207"
 EOF
+echo reload
 systemctl daemon-reload
+echo restart
 systemctl restart nix-daemon
+echo build!
 https_proxy=http://localhost:52207 http_proxy=http://localhost:52207 all_proxy=http://localhost:52207 nixos-rebuild switch --flake ".#$(hostname)"
