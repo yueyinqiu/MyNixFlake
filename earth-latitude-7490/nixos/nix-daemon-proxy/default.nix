4f796c5641632a3e814d92f85d43931b5b78b0cf
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 17:42:24 +0800

    fix

--- earth-latitude-7490/nixos/nix-daemon-proxy/default.nix
+++ earth-latitude-7490/nixos/nix-daemon-proxy/default.nix
@@ -1,23 +1,20 @@
 { config, lib, pkgs, ... }:
 let
-    certificates = pkgs.runCommand "nix-daemon-proxy-certificates" { } ''
+    ca = pkgs.runCommand "nix-daemon-proxy-create-certificate" { } ''
         mkdir -p $out
-        ${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:2048 -nodes \
-            -keyout $out/key.pem -out $out/cert.pem -days 3650 \
-            -subj "/CN=nix-daemon-proxy-certificates"
-        cat $out/cert.pem $out/key.pem > $out/ca.pem
+        HOME=$out ${pkgs.mitmproxy}/bin/mitmdump --set confdir=$out --version --headless
     '';
 in {
-    security.pki.certificateFiles = [ "${certificates}/cert.pem" ];
+    security.pki.certificateFiles = [ "${ca}/mitmproxy-ca-cert.pem" ];
 
-    systemd.services.github-proxy = {
+    systemd.services.nix-daemon-proxy = {
         description = "Proxy for nix-daemon";
         before = [ "nix-daemon.service" ];
         wantedBy = [ "multi-user.target" ];
         after = [ "network.target" ];
 
         serviceConfig = {
-            ExecStart = "${pkgs.mitmproxy}/bin/mitmdump -s ${./nix_daemon_proxy.py} --listen-port 52207 --certs ${certificates}/ca.pem";
+            ExecStart = "${pkgs.mitmproxy}/bin/mitmdump -s ${./nix_daemon_proxy.py} --listen-port 52207 --certs ${ca}/mitmproxy-ca.pem";
             Restart = "on-failure";
             RestartSec = "3s";
         };
