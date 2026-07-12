25b17a3a837855cf12487711da190d6d330ad198
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 23:58:35 +0800

    add substituters

--- earth-latitude-7490/nixos/default.nix
+++ earth-latitude-7490/nixos/default.nix
@@ -17,8 +17,12 @@
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nix.settings.substituters = [
+        "https://mirror.nju.edu.cn/nix-channels/store"
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://mirrors.cernet.edu.cn/nix-channels/store"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        # "https://mirrors.cqupt.edu.cn/nix-channels/store"
+        # "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
     ];
     nix.settings.trusted-users = [ "yueyinqiu" ];
