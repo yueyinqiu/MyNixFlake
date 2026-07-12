4a5f43ff2ae2bcd8ccd86ea9d32cca30b296f8b0
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 20:39:31 +0800

    re

--- earth-latitude-7490/nixos/nix-daemon-proxy/nix_daemon_proxy.py
+++ earth-latitude-7490/nixos/nix-daemon-proxy/nix_daemon_proxy.py
@@ -1,18 +1,22 @@
 from mitmproxy import http
 import logging
 
-
 logger = logging.getLogger(__name__)
+
+def http_connect(flow: http.HTTPFlow) -> None:
+    logger.info(f"[http_connect] {flow}")
+    if flow.request.pretty_host == "github.com":
+        flow.request.host = "gh-proxy.org"
+        flow.request.port = 443
 
 
 def request(flow: http.HTTPFlow) -> None:
-    logger.info(f"[REQUEST] {flow.request.url}")
-    if flow.request.pretty_host == "github.com":
+    logger.info(f"[request] {flow}")
+    if "github.com" in flow.request.url or flow.request.pretty_host == "github.com":
         flow.request.url = f"https://gh-proxy.org/{flow.request.url}"
         flow.request.host = "gh-proxy.org"
         flow.request.origin = "https://gh-proxy.org"
-        logger.info(f"[REWRITE TO] {flow.request.url}")
 
 
 def response(flow: http.HTTPFlow) -> None:
-    logger.info(f"[RESPONSE] {flow.request.url} -> {flow.response.status_code}")
+    logger.info(f"[response] {flow.request.url} -> {flow.response.status_code}")
