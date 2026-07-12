f4fa015722bf0d1b4944ac6de8aca924614531fd
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 17:24:08 +0800

    merge

--- earth-latitude-7490/nixos/nix-daemon-proxy/default.nix
+++ earth-latitude-7490/nixos/nix-daemon-proxy/default.nix
@@ -3,11 +3,12 @@ let
     certificates = pkgs.runCommand "nix-daemon-proxy-certificates" { } ''
         mkdir -p $out
         ${pkgs.openssl}/bin/openssl req -x509 -newkey rsa:2048 -nodes \
-            -keyout $out/key.key -out $out/pem.pem -days 3650 \
+            -keyout $out/key.pem -out $out/cert.pem -days 3650 \
             -subj "/CN=nix-daemon-proxy-certificates"
+        cat $out/cert.pem $out/key.pem > $out/ca.pem
     '';
 in {
-    security.pki.certificateFiles = [ "${certificates}/pem.pem" ];
+    security.pki.certificateFiles = [ "${certificates}/cert.pem" ];
 
     systemd.services.github-proxy = {
         description = "Proxy for nix-daemon";
@@ -16,7 +17,7 @@ in {
         after = [ "network.target" ];
 
         serviceConfig = {
-            ExecStart = "${pkgs.mitmproxy}/bin/mitmdump -s ${./nix_daemon_proxy.py} --listen-port 52207 --certs ${certificates}/pem.pem=${certificates}/key.key";
+            ExecStart = "${pkgs.mitmproxy}/bin/mitmdump -s ${./nix_daemon_proxy.py} --listen-port 52207 --certs ${certificates}/ca.pem";
             Restart = "on-failure";
             RestartSec = "3s";
         };
