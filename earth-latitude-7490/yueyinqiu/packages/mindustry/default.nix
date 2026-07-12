e2d24e6255565b4b26c067cea3f6d3b2b5101c35
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 00:16:45 +0800

    rm mdt

--- earth-latitude-7490/yueyinqiu/packages/mindustry/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/mindustry/default.nix
@@ -3,9 +3,4 @@
         mindustry
     ];
     my.r.mindustry = "mindustry";
-    
-    services.flatpak.packages = [
-        "flathub:app/com.github.Anuken.Mindustry/x86_64/stable"
-    ];
-    my.r.mindustry-flatpak = "flatpak run com.github.Anuken.Mindustry";
 }
