9dbbde9dd105337183e5209f1f8929855b786708
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 17:56:46 +0800

    try

--- earth-latitude-7490/nixos/nix-daemon-proxy/nix_daemon_proxy.py
+++ earth-latitude-7490/nixos/nix-daemon-proxy/nix_daemon_proxy.py
@@ -4,4 +4,9 @@ def request(flow: http.HTTPFlow) -> None:
     if flow.request.pretty_host == "github.com":
         flow.request.url = f"https://gh-proxy.org/{flow.request.url}"
         flow.request.headers["Host"] = "gh-proxy.org"
-        flow.request.headers["Origin"] = "https://gh-proxy.org"
+        if flow.request.method not in ("GET", "HEAD"):
+            flow.request.headers["Origin"] = "https://gh-proxy.org"
+
+def response(flow: http.HTTPFlow) -> None:
+    if flow.request.pretty_host == "github.com" and flow.response:
+        flow.response.stream = lambda chunk: chunk
