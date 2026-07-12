834f68111a973e43a5b0e6fc7139ba37898cae84
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 13:40:00 +0800

    copy fonts

--- earth-latitude-7490/yueyinqiu/packages/onlyoffice/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/onlyoffice/default.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
     home.packages = with pkgs; [
         onlyoffice-desktopeditors
     ];
     my.r.office = "onlyoffice-desktopeditors";
+
+    home.activation.copyCorefonts = lib.hm.dag.entryAfter [ "writeBoundary" "installPackages" ] ''
+        mkdir -p ~/.local/share/fonts
+        cp -u ${pkgs.corefonts}/share/fonts/truetype/* ~/.local/share/fonts/
+        chmod 644 ~/.local/share/fonts/*
+    '';
 }
