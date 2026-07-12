e8646a4c56c0eae84e30ead90b88f385dea2984e
yueyinqiu <yueyinqiu@outlook.com>
2026-07-12 19:26:41 +0800

    p

--- os-proxy.sh
+++ os-proxy.sh
@@ -1,7 +1,9 @@
 mkdir -p /run/systemd/system/nix-daemon.service.d/
 tee /run/systemd/system/nix-daemon.service.d/override.conf << EOF
 [Service]
-Environment="https_proxy=http://localhost:52207/"
+Environment="https_proxy=http://localhost:52207"
+Environment="http_proxy=http://localhost:52207"
+Environment="all_proxy=http://localhost:52207"
 EOF
 systemctl daemon-reload
 systemctl restart nix-daemon
