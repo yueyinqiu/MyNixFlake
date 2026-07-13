3e98f9b7b032ecf1e889c868e99d46c4a80decc4
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 18:20:28 +0800

    flatpak wemeet

--- earth-latitude-7490/yueyinqiu/packages/wemeet/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wemeet/default.nix
@@ -1,5 +1,13 @@
+# 一些窗口显示、摄像头会有问题
+# { pkgs, ... }: {
+#     my.r.wemeet = ''
+#         "${pkgs.wemeet}/bin/wemeet"
+#     '';
+# }
+
 { pkgs, ... }: {
-    my.r.wemeet = ''
-        "${pkgs.wemeet}/bin/wemeet"
-    '';
+    services.flatpak.packages = [
+        "flathub:app/com.tencent.wemeet/x86_64/stable"
+    ];
+    my.r.wemeet = "flatpak run com.tencent.wemeet";
 }
