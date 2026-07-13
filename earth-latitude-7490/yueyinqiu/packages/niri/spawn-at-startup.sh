50f148285ea25ceabec669222d5676bc4264a1a2
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 13:27:40 +0800

    upd

--- earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
+++ earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+niri msg action spawn -- r sing-box-nix-daemon-proxy
+niri msg action spawn -- r nix-daemon-proxy socks5h://127.0.0.1:23569
+
 niri msg action spawn -- FlClash
 niri msg action spawn -- qq
 niri msg action spawn -- r wechat
-
-niri msg action focus-workspace 2
