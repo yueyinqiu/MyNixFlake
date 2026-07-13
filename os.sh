5a38e93df0f4917fc24bd081b89e72ea585daa48
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 11:51:28 +0800

    al

--- os.sh
+++ os.sh
@@ -1,2 +1,2 @@
 # journalctl -u nix-daemon-proxy -f
-sudo all_proxy=socks5h://127.0.0.1:62751 nixos-rebuild switch --flake ".#$(hostname)"
+sudo all_proxy=socks5h://127.0.0.1:62751 http_proxy=socks5h://127.0.0.1:62751 https_proxy=socks5h://127.0.0.1:62751 nixos-rebuild switch --flake ".#$(hostname)"
