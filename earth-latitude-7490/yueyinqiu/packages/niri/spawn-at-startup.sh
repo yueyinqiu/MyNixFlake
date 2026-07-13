f824dfe667b95063bdda83db44d777a9eb41c258
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 17:25:34 +0800

    fix

--- earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
+++ earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # https://github.com/ghostty-org/ghostty/pull/11762
-niri msg action spawn -- ghostty --command="r sing-box-nix-daemon-proxy; exec $SHELL"
-niri msg action spawn -- ghostty --command="nix-daemon-proxy socks5h://127.0.0.1:23569; exec $SHELL"
+niri msg action spawn -- r ghostty --command="r sing-box-nix-daemon-proxy; exec $SHELL"
+niri msg action spawn -- r ghostty --command="nix-daemon-proxy socks5h://127.0.0.1:23569; exec $SHELL"
 
 niri msg action spawn -- r flclash
 niri msg action spawn -- r qq
