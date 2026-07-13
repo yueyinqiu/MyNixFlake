c8f91323e666ae43349d52ec21e20664c94b10b6
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 13:31:49 +0800

    r

--- earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
+++ earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-niri msg action spawn -- r sing-box-nix-daemon-proxy
-niri msg action spawn -- r nix-daemon-proxy socks5h://127.0.0.1:23569
+niri msg action spawn -- r nix-daemon-proxy
+niri msg action spawn -- nix-daemon-proxy socks5h://127.0.0.1:23569
 
 niri msg action spawn -- FlClash
 niri msg action spawn -- qq
--- earth-latitude-7490/yueyinqiu/packages/sing-box/nix-daemon-proxy/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/sing-box/nix-daemon-proxy/default.nix
@@ -7,9 +7,9 @@ in
     xdg.configFile."${directory}/outbounds.json.example".source = ./outbounds.json.example;
     xdg.configFile."${directory}/rules.json.example".source = ./rules.json.example;
 
-    my.r.sing-box-nix-daemon-proxy = "${pkgs.sing-box}/bin/sing-box run -C ${config.xdg.configHome}/${directory} -D ${config.xdg.configHome}/${directory}";
-    my.r.sing-box-nix-daemon-proxy-manage = ''
-        echo export all_proxy=socks5h://127.0.0.1:23569
-        echo https://metacubex.github.io/metacubexd/#/setup?http=true&hostname=127.0.0.1&port=62751
+    my.r.nix-daemon-proxy = "${pkgs.sing-box}/bin/sing-box run -C ${config.xdg.configHome}/${directory} -D ${config.xdg.configHome}/${directory}";
+    my.r.nix-daemon-proxy-manage = ''
+        echo "export all_proxy=socks5h://127.0.0.1:23569"
+        echo "https://metacubex.github.io/metacubexd/#/setup?http=true&hostname=127.0.0.1&port=62751"
     '';
 }
