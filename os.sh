ab8a79a07ddadb024e8feedeee38c572a40a00fe
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 11:27:48 +0800

    gogoo

--- os.sh
+++ os.sh
@@ -1,1 +1,2 @@
-nixos-rebuild switch --flake ".#$(hostname)"
+# journalctl -u nix-daemon-proxy -f
+all_proxy=socks5h://127.0.0.1:52138 http_proxy=socks5h://127.0.0.1:52138 https_proxy=socks5h://127.0.0.1:52138 nixos-rebuild switch --flake ".#$(hostname)"
