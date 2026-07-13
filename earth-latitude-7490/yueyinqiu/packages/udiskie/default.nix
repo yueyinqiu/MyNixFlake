b54ffb1da842dcc5d576b0824442c454120f2162
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 17:55:01 +0800

    udisksctl

--- earth-latitude-7490/yueyinqiu/packages/udiskie/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/udiskie/default.nix
@@ -1,4 +1,12 @@
 { pkgs, config, ... }: {
     services.udiskie.enable = true;
     home.file."udiskie".source = config.lib.file.mkOutOfStoreSymlink "/run/media/yueyinqiu";
+
+    my.r.udisksctl-status = ''
+        udisksctl status
+    '';
+    my.r.udisksctl-power-off = ''
+        udisksctl unmount -b "/dev/$@"
+        udisksctl power-off -b "/dev/$@"
+    '';
 }
