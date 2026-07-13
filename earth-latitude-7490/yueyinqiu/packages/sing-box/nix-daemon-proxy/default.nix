013729bd83e9705ba978eb047c204ea4816851d6
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 12:58:27 +0800

    nix-daemon-proxy-manage

--- earth-latitude-7490/yueyinqiu/packages/sing-box/nix-daemon-proxy/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/sing-box/nix-daemon-proxy/default.nix
@@ -7,5 +7,9 @@ in
     xdg.configFile."${directory}/outbounds.json.example".source = ./outbounds.json.example;
     xdg.configFile."${directory}/rules.json.example".source = ./rules.json.example;
 
-    my.r.nixos-rebuild-proxy = "${pkgs.sing-box}/bin/sing-box run -C ${config.xdg.configHome}/${directory} -D ${config.xdg.configHome}/${directory}";
+    my.r.nix-daemon-proxy = "${pkgs.sing-box}/bin/sing-box run -C ${config.xdg.configHome}/${directory} -D ${config.xdg.configHome}/${directory}";
+    my.r.nix-daemon-proxy-manage = ''
+        echo socks5://127.0.0.1:23569
+        echo http://127.0.0.1:62751
+    '';
 }
