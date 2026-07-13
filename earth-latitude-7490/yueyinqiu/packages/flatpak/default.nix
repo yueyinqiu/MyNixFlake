301c1173aa3870fdad992a713af413548583d90d
yueyinqiu <yueyinqiu@outlook.com>
2026-07-13 21:23:53 +0800

    add flatpak remote

--- earth-latitude-7490/yueyinqiu/packages/flatpak/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/flatpak/default.nix
@@ -5,12 +5,9 @@
 
     services.flatpak = {
         enable = true;
-        # remotes = {
-        #     "flathub" = "https://mirror.sjtu.edu.cn/flathub/flathub.flatpakrepo";
-        # };
-        # preInstallCommand = ''
-        #     flatpak --user remote-modify flathub --url=https://mirror.sjtu.edu.cn/flathub
-        # '';
+        remotes = {
+            "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo"
+        };
     };
 }
 
