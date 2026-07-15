e447c5b39fa18578b04f5e0648f693416491575a
yueyinqiu <yueyinqiu@outlook.com>
2026-07-15 12:30:10 +0800

    x

--- earth-latitude-7490/nixos/nix-daemon-proxy/server.cs
+++ earth-latitude-7490/nixos/nix-daemon-proxy/server.cs
@@ -12,14 +12,17 @@ Console.WriteLine($"Started.");
 var socket = "/run/nix-daemon-proxy.sock";
 var overrideConf = new FileInfo("/run/systemd/system/nix-daemon.service.d/override.conf");
 
+Console.WriteLine($"CreateBuilder.");
 var builder = WebApplication.CreateBuilder();
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenUnixSocket(socket);
 });
 
+Console.WriteLine($"Build.");
 var app = builder.Build();
 
+Console.WriteLine($"MapPost.");
 app.MapPost("/", async ([FromQuery] string proxy = "") =>
 {
     await File.WriteAllTextAsync(overrideConf.FullName,
@@ -32,6 +35,7 @@ app.MapPost("/", async ([FromQuery] string proxy = "") =>
     await Cli.Wrap("systemctl").WithArguments(["restart", "nix-daemon"]).ExecuteBufferedAsync();
 });
 
+Console.WriteLine($"Register.");
 app.Lifetime.ApplicationStarted.Register(async () =>
 {
     Debug.Assert(RuntimeInformation.IsOSPlatform(OSPlatform.Linux));
@@ -39,7 +43,9 @@ app.Lifetime.ApplicationStarted.Register(async () =>
     await Cli.Wrap("chown").WithArguments(["root:nix-daemon-proxy", socket]).ExecuteBufferedAsync();
 });
 
+Console.WriteLine($"Create.");
 overrideConf.Directory?.Create();
+Console.WriteLine($"Exists.");
 if (File.Exists(socket))
     File.Delete(socket);
 
