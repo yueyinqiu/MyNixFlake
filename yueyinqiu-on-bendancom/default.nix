bc6e58a70c252f88783263e5f729b222042d00aa
yueyinqiu <yueyinqiu@outlook.com>
2026-07-15 14:56:02 +0800

    add git

--- yueyinqiu-on-bendancom/default.nix
+++ yueyinqiu-on-bendancom/default.nix
@@ -6,14 +6,6 @@
 
     nixpkgs.config.allowUnfree = true;
 
-    home.packages = with pkgs; [
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        noto-fonts-color-emoji
-        corefonts
-    ];
-
     imports = [ ./packages ];
 
     home.stateVersion = "26.05";
