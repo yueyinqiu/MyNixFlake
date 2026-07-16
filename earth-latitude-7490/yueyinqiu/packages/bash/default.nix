b598d247ef0e34f2f0ba16dfe2e62970ed7f4227
yueyinqiu <yueyinqiu@outlook.com>
2026-07-16 08:55:16 +0800

    extra

--- earth-latitude-7490/yueyinqiu/packages/bash/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/bash/default.nix
@@ -1,5 +1,12 @@
 { ... }: {
-  # programs.bash.enable = true;
+  programs.bash = {
+    enable = true;
+    bashrcExtra = ''
+      export GTK_IM_MODULE=fcitx
+      export QT_IM_MODULE=fcitx
+      export XMODIFIERS=@im=fcitx
+    '';
+  };
   my.r."bash" = ''
     bash "$@"
   '';
