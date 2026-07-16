02b7536830249518776f4ef50836aafac2e66771
yueyinqiu <yueyinqiu@outlook.com>
2026-07-16 14:23:29 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
@@ -1,29 +1,37 @@
 { ... }: {
-programs.firefox = {
-  enable = true;
+  programs.firefox = {
+    enable = true;
 
-  profiles.default = {
-    search.default = "bing";
-    search.force = true;
+    profiles.default = {
+      search = {
+        force = true;
+        default = "bing";
+      };
 
-    settings = {
-      "browser.startup.homepage" = "about:blank";
-      "browser.startup.page" = 0;
+      settings = {
+        "browser.startup.homepage" = "about:blank";
+        "browser.startup.page" = 0;
 
-      "network.proxy.type" = 1;
-      "network.proxy.socks" = "127.0.0.1";
-      "network.proxy.socks_port" = 7890;
-    };
+        "network.proxy.type" = 1;
+        "network.proxy.socks" = "127.0.0.1";
+        "network.proxy.socks_port" = 7890;
+      };
 
-    bookmarks = [
-      {
-        name = "Bookmarks Toolbar"; 
-        toolbar = true;
-        bookmarks = [
-          { name = "GitHub"; url = "https://github.com"; }
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "Bookmarks Toolbar";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "GitHub";
+                url = "https://github.com";
+              }
+            ];
+          }
         ];
-      }
-    ];
+      };
+    };
   };
-};
 }
