954bce90933dead8449dce4633bfa245d0d62210
yueyinqiu <yueyinqiu@outlook.com>
2026-07-16 14:14:36 +0800

    test

--- earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
@@ -1,3 +1,29 @@
 { ... }: {
-  programs.firefox.enable = true;
+programs.firefox = {
+  enable = true;
+
+  profiles.default = {
+    search.default = "Bing";
+    search.force = true;
+
+    settings = {
+      "browser.startup.homepage" = "about:blank";
+      "browser.startup.page" = 0;
+
+      "network.proxy.type" = 1;
+      "network.proxy.socks" = "127.0.0.1";
+      "network.proxy.socks_port" = 7890;
+    };
+
+    bookmarks = [
+      {
+        name = "Bookmarks Toolbar"; # 丢进火狐顶部的书签栏
+        toolbar = true;
+        bookmarks = [
+          { name = "GitHub"; url = "https://github.com"; }
+        ];
+      }
+    ];
+  };
+};
 }
