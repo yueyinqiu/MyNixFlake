165e1d1caf01db1ff76887c093f4042fa39c8f9c
yueyinqiu <yueyinqiu@outlook.com>
2026-07-16 15:20:01 +0800

    x

--- earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
@@ -3,6 +3,8 @@
     enable = true;
 
     profiles.default = {
+      name = "default";
+
       search = {
         force = true;
         default = "bing";
@@ -17,47 +19,75 @@
         "network.proxy.socks_port" = 7890;
 
         "browser.toolbars.bookmarks.visibility" = "always";
-      };
+        "browser.download.autohideButton" = false;
+        "browser.uiCustomization.state" = builtins.toJSON {
+          currentVersion = 24;
 
-      extensions.packages = with nur.repos.rycee.firefox-addons; [
-        bitwarden
-      ];
-
-      bookmarks = {
-        force = true;
-        settings = [
-
-          {
-            name = "wikipedia";
-            tags = [ "wiki" ];
-            keyword = "wiki";
-            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-          }
-          {
-            name = "kernel.org";
-            url = "https://www.kernel.org";
-          }
-          "separator"
-          {
-            name = "Nix sites";
-            toolbar = true;
-            bookmarks = [
-              {
-                name = "homepage";
-                url = "https://nixos.org/";
-              }
-              {
-                name = "wiki";
-                tags = [
-                  "wiki"
-                  "nix"
-                ];
-                url = "https://wiki.nixos.org/";
-              }
+          placements = {
+            widget-overflow-fixed-list = [ ];
+            unified-extensions-area = [ ];
+            toolbar-menubar = [ "menubar-items" ];
+            TabsToolbar = [
+              "firefox-view-button"
+              "tabbrowser-tabs"
+              "new-tab-button"
+              "alltabs-button"
             ];
-          }
-        ];
+            vertical-tabs = [ ];
+            PersonalToolbar = [
+              "import-button"
+              "personal-bookmarks"
+            ];
+
+            nav-bar = [
+              "back-button"
+              "forward-button"
+              "stop-reload-button"
+              "customizableui-special-spring1"
+              "vertical-spacer"
+              "urlbar-container"
+              "customizableui-special-spring2"
+              "downloads-button"
+              "history-panelmenu"
+              "fxa-toolbar-menu-button"
+              "reset-pbm-toolbar-button"
+              "unified-extensions-button"
+            ];
+          };
+        };
       };
+    };
+
+    extensions.packages = with nur.repos.rycee.firefox-addons; [
+      bitwarden
+    ];
+
+    bookmarks = {
+      force = true;
+      settings = [
+        {
+          name = "wikipedia";
+          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+        {
+          name = "kernel.org";
+          url = "https://www.kernel.org";
+        }
+        {
+          name = "Nix sites";
+          toolbar = true;
+          bookmarks = [
+            {
+              name = "homepage";
+              url = "https://nixos.org/";
+            }
+            {
+              name = "wiki";
+              url = "https://wiki.nixos.org/";
+            }
+          ];
+        }
+      ];
     };
   };
 }
