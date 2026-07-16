290edf1e461252fcbc74802b4b9426778a5d7e66
yueyinqiu <yueyinqiu@outlook.com>
2026-07-16 15:36:42 +0800

    bookmarks

--- earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/firefox/default.nix
@@ -33,24 +33,64 @@
         force = true;
         settings = [
           {
-            name = "wikipedia";
-            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+            name = "PDF转JPG——免费在线PDF转成图片格式";
+            url = "www.pdfdo.com/pdf-to-image.aspx";
           }
           {
-            name = "kernel.org";
-            url = "https://www.kernel.org";
+            name = "Snappify";
+            url = "https://snappify.com/dashboard";
+          }
+          {
+            name = "SVG转换为PDF - 迅捷，在线，免费 - PDF24";
+            url = "https://tools.pdf24.org/zh/svg-to-pdf";
+          }
+          {
+            name = "在线免费文字转语音 - TTSMaker官网 | 马克配音";
+            url = "https://ttsmaker.cn";
+          }
+          {
+            name = "Snapmail";
+            url = "https://snapmail.cc/#/emailList/levoczid@snapmail.cc";
+          }
+          {
+            name = "Yijing Dao";
+            url = "https://www.biroco.com/yijing/glossary.htm";
+          }
+          {
+            name = "百度网盘空间站";
+            url = "https://www.nccnxta.cn/pan/baidu";
+          }
+          {
+            name = "微信公众平台";
+            url = "https://mp.weixin.qq.com/";
+          }
+          {
+            name = "TDoGMC Studio - 皮肤编辑器 | Minecraft皮肤制作 | 在线编辑，无需下载";
+            url = "https://studio.tdogmc.cn/?lang=zh-CN";
           }
           {
             name = "Nix sites";
             toolbar = true;
             bookmarks = [
               {
-                name = "homepage";
-                url = "https://nixos.org/";
+                name = "微信开发者平台";
+                url = "https://developers.weixin.qq.com/console/product/mp/wx01c09572ce8cefa2?tab1=basicInfo&tab2=apiMonitoring";
               }
               {
-                name = "wiki";
-                url = "https://wiki.nixos.org/";
+                name = "SakuraFrp";
+                url = "https://www.natfrp.com/user/";
+              }
+              {
+                name = "云盘 - 飞书云文档";
+                url = "https://jcnljw0jyizz.feishu.cn/drive/me/";
+              }
+              {
+                name = "随机端口生成器 - Random Port Generator - 查获 chahuo.com";
+                url = "https://www.chahuo.com/random-port-generator.html";
+              }
+              {
+                name = "NixOS Search - Packages";
+                url = "https://search.nixos.org/packages";
               }
             ];
           }
