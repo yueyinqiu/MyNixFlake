387851e0f10e7ff337c6a33dae2c92fd90e6a759
yueyinqiu <yueyinqiu@outlook.com>
2026-07-17 18:01:26 +0800

    rr

--- earth-latitude-7490/yueyinqiu/packages/_7z/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/_7z/default.nix
@@ -1,15 +1,19 @@
 { pkgs, ... }: {
+  home.packages = with pkgs; [
+    _7zz
+  ];
+
   my.r."7z" = ''
-    "${pkgs._7zz}/bin/7zz" "$@"
+    7zz "$@"
   '';
   my.r."7z-a" = ''
-    "${pkgs._7zz}/bin/7zz" a "$@"
+    7zz a "$@"
   '';
   my.r."7z-x" = ''
     target="$1"
     name="$(basename "$target")"
     name="''${name%.*}"
     mkdir -p "$name"
-    "${pkgs._7zz}/bin/7zz" x "$target" -o"$name"
+    7zz x "$target" -o"$name"
   '';
 }
--- earth-latitude-7490/yueyinqiu/packages/avizo/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/avizo/default.nix
@@ -1,3 +1,22 @@
 { ... }: {
   services.avizo.enable = true;
+
+  my.r.avizo-speaker-up = ''
+    volumectl -u up
+  '';
+  my.r.avizo-speaker-down = ''
+    volumectl -u down
+  '';
+  my.r.avizo-speaker-mute = ''
+    volumectl toggle-mute
+  '';
+  my.r.avizo-microphone-mute = ''
+    volumectl -m toggle-mute
+  '';
+  my.r.avizo-light-up = ''
+    lightctl up
+  '';
+  my.r.avizo-light-down = ''
+    lightctl down
+  '';
 }
