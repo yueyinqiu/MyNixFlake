b1fab6e295bc7e81d09c4e04deaae46b4156ade1
yueyinqiu <yueyinqiu@outlook.com>
2026-07-17 21:21:57 +0800

    r expect-bash-run r sing-box-nix-daemon-proxy

--- earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
+++ earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # https://github.com/ghostty-org/ghostty/pull/11762
-niri msg action spawn -- r ghostty --command="set -x; r sing-box-nix-daemon-proxy; exec $SHELL"
-niri msg action spawn -- r ghostty --command="set -x; nix-daemon-proxy socks5h://127.0.0.1:23569; exec $SHELL"
+niri msg action spawn -- r ghostty --command="r expect-bash-run r sing-box-nix-daemon-proxy"
+niri msg action spawn -- r ghostty --command="r expect-bash-run nix-daemon-proxy socks5h://127.0.0.1:23569"
 
 niri msg action spawn -- r flclash
 niri msg action spawn -- r qq
