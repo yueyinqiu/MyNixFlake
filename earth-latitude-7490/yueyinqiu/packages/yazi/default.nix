1f5fa4fc136ca619ff9c156912ec3b8e8a54fc02
yueyinqiu <yueyinqiu@outlook.com>
2026-07-19 10:53:17 +0800

    yazi

--- earth-latitude-7490/yueyinqiu/packages/yazi/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/yazi/default.nix
@@ -1,6 +1,9 @@
-{ ... }: {
+{ lib, ... }: {
   programs.yazi.enable = true;
-
+  programs.yazi.settings = {
+    yazi = lib.importTOML ./yazi.toml;
+    keymap = lib.importTOML ./keymap.toml;
+  };
   my.r.yazi = ''
     yazi "$@"
   '';
