a3df6d786018509e1c6582f779dfb034ade9891d
yueyinqiu <yueyinqiu@outlook.com>
2026-07-19 12:19:10 +0800

    confirm-close-surface

--- earth-latitude-7490/yueyinqiu/packages/ghostty/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/ghostty/default.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    ghostty
-  ];
+{ ... }: {
+  programs.ghostty.enable = true;
+  programs.ghostty.settings = {
+    confirm-close-surface = false;
+  };
 
   my.r.ghostty = ''
     ghostty "$@"
