b87b1dcc70a48583ad5cac17eca6df4994f4abb2
yueyinqiu <yueyinqiu@outlook.com>
2026-07-22 22:25:50 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/sub2sing-box/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/sub2sing-box/default.nix
@@ -4,32 +4,26 @@ let
   mihomo2singbox = pkgs.python3.pkgs.buildPythonApplication {
     pname = "mihomo2singbox";
     version = "0.1.0";
-
     format = "pyproject";
-
     src = pkgs.fetchFromGitHub {
       owner = "n-WN";
       repo = "mihomo2singbox";
       rev = "main";
       hash = "sha256-nVsvTuz4zmrQDwsEExv+XNzKI79MdwtcJQAZ6eciUxw=";
     };
-
     nativeBuildInputs = with pkgs.python3.pkgs; [
       hatchling
     ];
-
     propagatedBuildInputs = with pkgs.python3.pkgs; [
       pyyaml
     ];
-
     meta = with pkgs.lib; {
-      description = "Mihomo (ClashMeta) 配置转换为 Sing-box 配置的转换工具";
+      description = "mihomo2singbox";
       homepage = "https://github.com/n-WN/mihomo2singbox";
       license = licenses.mit;
       mainProgram = "mihomo2singbox";
     };
   };
-
 in {
   home.packages = [
     mihomo2singbox
