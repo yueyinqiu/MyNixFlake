9b54d0ced1b92174dde9ff185e4b4c1f79ae564f
yueyinqiu <yueyinqiu@outlook.com>
2026-07-22 21:40:14 +0800

    add subc

---
+++ earth-latitude-7490/yueyinqiu/packages/sub2sing-box/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+let
+  sub2sing-box = pkgs.buildGoModule rec {
+    pname = "sub2sing-box";
+    version = "0.0.11";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "bestnite";
+      repo = "sub2sing-box";
+      rev = "v${version}";
+      hash = "sha256-5A0cK+M2NylM/T/9jIeY1vN6i2RkS3K8O2K0wO2K0wO=";
+    };
+
+    vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+
+    meta = with pkgs.lib; {
+      description = "将节点和订阅转换为 sing-box 配置的工具";
+      homepage = "https://github.com/bestnite/sub2sing-box";
+      license = licenses.mit;
+      mainProgram = "sub2sing-box";
+    };
+  };
+
+in {
+  home.packages = [
+    sub2sing-box
+  ];
+}
