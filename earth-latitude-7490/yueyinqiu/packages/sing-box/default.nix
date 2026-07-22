9ccb356bdba7fe8261da9724201721d1337c9d05
yueyinqiu <yueyinqiu@outlook.com>
2026-07-22 16:00:33 +0800

    sing-box

--- earth-latitude-7490/yueyinqiu/packages/sing-box/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/sing-box/default.nix
@@ -2,9 +2,12 @@
   imports = [
     ./nix-daemon-proxy
   ];
+  home.packages = with pkgs; [
+    sing-box
+  ];
 
   my.r.sing-box = ''
-    "${pkgs.sing-box}/bin/sing-box" "$@"
+    sing-box "$@"
   '';
   my.r.cd-sing-box-config = ''
     cd "${config.xdg.configHome}/sing-box"
