098cf9e0d214135cef143712e525c647df7bf61b
yueyinqiu <yueyinqiu@outlook.com>
2026-07-23 10:46:39 +0800

    nix daemon proxy !

--- earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
+++ earth-latitude-7490/yueyinqiu/packages/niri/spawn-at-startup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # https://github.com/ghostty-org/ghostty/pull/11762
-niri msg action spawn -- ghostty --command="r expect-bash-run r sing-box-nix-daemon-proxy"
-niri msg action spawn -- ghostty --command="r expect-bash-run nix-daemon-proxy socks5h://127.0.0.1:23569"
 
 niri msg action spawn -- FlClash
 niri msg action spawn -- qq
--- earth-latitude-7490/yueyinqiu/packages/nix-daemon-proxy/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/nix-daemon-proxy/default.nix
@@ -7,4 +7,27 @@
     port = 23569;
     manager = 62751;
   };
+
+  systemd.user.services.nix-daemon-proxy = {
+    Unit = {
+      Description = "Set nix-daemon proxy";
+      After = [ "sing-box-nix-daemon.service" ];
+      Wants = [ "sing-box-nix-daemon.service" ];
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "nix-daemon-proxy socks5h://127.0.0.1:23569";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      StartLimitIntervalSec = 300;
+      StartLimitBurst = 60;
+    };
+  };
+
+  my.r.set-nix-daemon-proxy = ''
+    systemctl --user restart nix-daemon-proxy
+  '';
 }
