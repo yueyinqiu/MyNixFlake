fb37ffff86c96eb271ecb8d2e4bc4512cd1a8eef
yueyinqiu <yueyinqiu@outlook.com>
2026-07-24 18:15:54 +0800

    sethome, fuck you wps

--- earth-latitude-7490/yueyinqiu/packages/wps/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wps/default.nix
@@ -5,6 +5,6 @@
 
   # https://github.com/Beriholic/nix-wpsoffice-cn/issues/1#issuecomment-3706931491
   my.r.wps = ''
-    GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" wps "$@"
+    HOME=/home/yueyinqiu/.wps GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" wps "$@"
   '';
 }
