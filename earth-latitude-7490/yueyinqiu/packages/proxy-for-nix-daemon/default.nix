fda56cf383f3c880b6748ed501df9d2e614c6c25
yueyinqiu <yueyinqiu@outlook.com>
2026-07-26 00:29:39 +0800

    s

--- earth-latitude-7490/yueyinqiu/packages/proxy-for-nix-daemon/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/proxy-for-nix-daemon/default.nix
@@ -27,7 +27,11 @@
     };
   };
 
-  my.r.set-nix-daemon-proxy = ''
+  my.r.proxy-for-nix-daemon-switch = ''
+    nix-daemon-proxy socks5h://127.0.0.1:23569
+  '';
+
+  my.r.proxy-for-nix-daemon-check-block = ''
     systemctl --user restart proxy-for-nix-daemon
   '';
 }
