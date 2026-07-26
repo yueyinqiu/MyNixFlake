07b291d1131099b2d24dbb26ff588367eb0f4329
yueyinqiu <yueyinqiu@outlook.com>
2026-07-26 22:32:02 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/wps/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wps/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   # https://github.com/Beriholic/nix-wpsoffice-cn/issues/1#issuecomment-3706931491
   # https://bbs.wps.cn/topic/61831
+  # Don't Login! It will create a fucking Documents directory!
   my.r.wps = ''
     GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" HOME="$HOME/.wps" ${pkgs.wpsoffice-cn}/bin/wps
   '';
