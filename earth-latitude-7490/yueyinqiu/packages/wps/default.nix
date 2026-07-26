cac8b9c2c4b62e5889f228b31c89a7a788e7269e
yueyinqiu <yueyinqiu@outlook.com>
2026-07-26 22:31:41 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/wps/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wps/default.nix
@@ -2,6 +2,6 @@
   # https://github.com/Beriholic/nix-wpsoffice-cn/issues/1#issuecomment-3706931491
   # https://bbs.wps.cn/topic/61831
   my.r.wps = ''
-    GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" HOME="$HOME/.wps" XDG_DOCUMENTS_DIR="$HOME/.wps/Documents" ${pkgs.wpsoffice-cn}/bin/wps
+    GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" HOME="$HOME/.wps" ${pkgs.wpsoffice-cn}/bin/wps
   '';
 }
