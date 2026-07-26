0016b37c860745786cd052bb6efa459aa1415f4f
yueyinqiu <yueyinqiu@outlook.com>
2026-07-26 22:18:31 +0800

    only r

--- earth-latitude-7490/yueyinqiu/packages/wps/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wps/default.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    wpsoffice-cn
-  ];
-
   # https://github.com/Beriholic/nix-wpsoffice-cn/issues/1#issuecomment-3706931491
   # https://bbs.wps.cn/topic/61831
   my.r.wps = ''
-    HOME="$HOME/.wps" wps
+    GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" HOME="$HOME/.wps" ${pkgs.wpsoffice-cn}/bin/wps
   '';
 }
