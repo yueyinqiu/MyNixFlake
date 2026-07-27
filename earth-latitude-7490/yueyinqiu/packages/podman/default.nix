b5e2c51c18d52c947d4aa55f0bcaf80a3cd05ab8
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 23:08:48 +0800

    proot

--- earth-latitude-7490/yueyinqiu/packages/podman/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/podman/default.nix
@@ -2,4 +2,13 @@
   home.packages = with pkgs; [
     podman-compose
   ];
+
+  my.podman = [
+    ''
+      % podman
+
+      # run podman, a daemonless container engine that lets you manage containers, pods, and images.
+      podman <args>
+    ''
+  ];
 }
--- earth-latitude-7490/yueyinqiu/packages/proot/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/proot/default.nix
@@ -2,4 +2,13 @@
   home.packages = with pkgs; [
     proot
   ];
+
+  my.proot = [
+    ''
+      % proot
+
+      # run proot, a user-space implementation of chroot, mount --bind, and binfmt_misc.
+      proot <args>
+    ''
+  ];
 }
