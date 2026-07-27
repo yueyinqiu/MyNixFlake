94718ee4f54fe175d337b06eea6877542dcb8180
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 21:00:50 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/navi/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/navi/default.nix
@@ -1,13 +1,13 @@
 { lib, config, ... }: {
-  programs.navi.enable = true;
-  programs.navi.enableBashIntegration = true;
-
   options.my.navi-cheats = lib.mkOption {
     type = lib.types.attrsOf lib.types.str;
     default = { };
   };
 
   config = {
+    programs.navi.enable = true;
+    programs.navi.enableBashIntegration = true;
+
     home.file = lib.mapAttrs' (name: value: {
       name = ".local/share/navi/cheats/${name}.cheat.md";
       value = {
