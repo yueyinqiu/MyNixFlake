a0c0acf01b27306682bf5b04c943c33963d515d6
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 22:29:15 +0800

    gh

--- earth-latitude-7490/yueyinqiu/packages/fuzzel/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/fuzzel/default.nix
@@ -1,3 +1,11 @@
 { ... }: {
   programs.fuzzel.enable = true;
+  my.navi-cheats = [
+    ''
+      % fuzzel
+
+      # run fuzzel, a Wayland-native application launcher and fuzzy finder, inspired by rofi and dmenu.
+      fuzzel
+    ''
+  ];
 }
--- earth-latitude-7490/yueyinqiu/packages/ghostty/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/ghostty/default.nix
@@ -3,4 +3,14 @@
   programs.ghostty.settings = {
     confirm-close-surface = false;
   };
+  shell-integration-features = "ssh-env";
+  
+  my.navi-cheats = [
+    ''
+      % ghostty
+
+      # run ghostty, a fast, feature-rich, and cross-platform terminal emulator that uses platform-native UI and GPU acceleration.
+      ghostty
+    ''
+  ];
 }
