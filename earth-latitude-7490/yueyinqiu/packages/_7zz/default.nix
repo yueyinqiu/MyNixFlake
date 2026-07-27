3fa3b238dfa59c62170084add4e9473ee17abc23
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 20:09:32 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/_7zz/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/_7zz/default.nix
@@ -1,12 +1,19 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     _7zz
-    (writeShellApplication "7zz-x" ''
-      target="$1"
-      name="$(basename "$target")"
-      name="''${name%.*}"
-      mkdir -p "$name"
-      7zz x "$target" -o"$name"
-    '')
+
+    (writeShellApplication {
+      name = "7zz-x";
+      runtimeInputs = [
+        pkgs._7zz
+      ];
+      text = ''
+        target="$1"
+        name="$(basename "$target")"
+        name="''${name%.*}"
+        mkdir -p "$name"
+        7zz x "$target" -o"$name"
+      '';
+    })
   ];
 }
