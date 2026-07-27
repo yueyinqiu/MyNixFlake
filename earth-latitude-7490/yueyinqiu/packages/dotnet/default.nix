87c1689d6cf9f9d8ff5ee9cbf46417f0d29b1aff
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 22:02:37 +0800

    dotnet edge

--- earth-latitude-7490/yueyinqiu/packages/dotnet/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/dotnet/default.nix
@@ -2,4 +2,20 @@
   home.packages = with pkgs; [
     dotnetCorePackages.sdk_10_0
   ];
+
+  my.navi-cheats = [
+    ''
+      % dotnet
+
+      # run dotnet, a free, open-source, cross-platform framework for building modern apps and powerful cloud services.
+      dotnet <args>
+    ''
+    ''
+      % dotnet
+
+      # run a dotnet file-based app.
+      dotnet run "<file>"
+      $file: find . -maxdepth 1 -type f -name "*.cs"
+    ''
+  ];
 }
--- earth-latitude-7490/yueyinqiu/packages/edge/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    microsoft-edge
-  ];
-
-  my.r.edge = ''
-    microsoft-edge --proxy-server=localhost:7890
-  '';
-}
