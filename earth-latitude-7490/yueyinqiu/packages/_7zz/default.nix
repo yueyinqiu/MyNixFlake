70e455a3270139bed59c5c0a533a3eea74934729
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 20:08:25 +0800

    writeShellApplication?

--- earth-latitude-7490/yueyinqiu/packages/_7zz/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/_7zz/default.nix
@@ -1,16 +1,12 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     _7zz
+    (writeShellApplication "7zz-x" ''
+      target="$1"
+      name="$(basename "$target")"
+      name="''${name%.*}"
+      mkdir -p "$name"
+      7zz x "$target" -o"$name"
+    '')
   ];
-
-  my.r."7zz-a" = ''
-    7zz a "$@"
-  '';
-  my.r."7zz-x" = ''
-    target="$1"
-    name="$(basename "$target")"
-    name="''${name%.*}"
-    mkdir -p "$name"
-    7zz x "$target" -o"$name"
-  '';
 }
