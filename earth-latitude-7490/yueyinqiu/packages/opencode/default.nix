2bc3b95e96f2545af3a9bc2d119c5ba017440f13
yueyinqiu <yueyinqiu@outlook.com>
2026-07-27 23:02:05 +0800

    r

--- earth-latitude-7490/yueyinqiu/packages/opencode/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/opencode/default.nix
@@ -2,4 +2,20 @@
   home.packages = with pkgs; [
     opencode
   ];
+
+  my.navi-cheats = [
+    ''
+      % opencode
+
+      # start a new opencode interactive session
+      opencode
+    ''
+    ''
+      % opencode
+
+      # resume a specific opencode session
+      opencode -s "<session>"
+      $ session: opencode session list | grep '^ses_' --- --column 1
+    ''
+  ];
 }
