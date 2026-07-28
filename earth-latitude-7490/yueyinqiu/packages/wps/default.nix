d973fe2c805211175790d98c7ee628f702b1aca7
yueyinqiu <yueyinqiu@outlook.com>
2026-07-28 22:29:09 +0800

    wps

--- earth-latitude-7490/yueyinqiu/packages/wps/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wps/default.nix
@@ -2,11 +2,24 @@
   # https://github.com/Beriholic/nix-wpsoffice-cn/issues/1#issuecomment-3706931491
   # https://bbs.wps.cn/topic/61831
   # Don't Login! It will create a fucking Documents directory!
-  my.r.wps = ''
-    GTK_IM_MODULE="fcitx" QT_IM_MODULE="fcitx" SDL_IM_MODULE="fcitx" HOME="$HOME/.wps" ${pkgs.wpsoffice-cn}/bin/wps
-  '';
+
+  home.packages = [
+    (pkgs.symlinkJoin {
+      name = "wps-fcitx-fake-home";
+      paths = [ pkgs.wpsoffice-cn ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/wps \
+          --set GTK_IM_MODULE "fcitx" \
+          --set QT_IM_MODULE "fcitx" \
+          --set SDL_IM_MODULE "fcitx" \
+          --set HOME "$HOME/.wps"
+      '';
+    })
+  ];
+  
   my.navi-cheats.wps = ''
     # run wps, a free all-in-one office suite for Microsoft Word, Excel, PowerPoint, and PDF files.
-    r wps
+    wps
   '';
 }
