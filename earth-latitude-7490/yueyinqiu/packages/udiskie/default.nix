85f38c83fd5242fac1b46ae5fcdf289098a363e5
yueyinqiu <yueyinqiu@outlook.com>
2026-07-28 22:25:00 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/udiskie/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/udiskie/default.nix
@@ -2,11 +2,6 @@
   services.udiskie.enable = true;
   home.file."udiskie".source = config.lib.file.mkOutOfStoreSymlink "/run/media/yueyinqiu";
 
-  my.r.udiskie-power-off = ''
-    udisksctl unmount -b "/dev/$@"
-    udisksctl power-off -b "/dev/$@"
-  '';
-  
   my.navi-cheats.udiskie = ''
     $ device: lsblk -l -n -o NAME --- --map my-bash-escape-std
 
@@ -16,5 +11,4 @@
     # power off a disk device
     udisksctl power-off -b <device>
   '';
-  
 }
--- earth-latitude-7490/yueyinqiu/packages/wechat/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/wechat/default.nix
@@ -1,11 +1,25 @@
 { pkgs, ... }: {
-  # https://zhuanlan.zhihu.com/p/2051087997395808628
-  my.r.wechat = ''
-    env WAYLAND_DISPLAY= DISPLAY=:0 QT_QPA_PLATFORM=xcb GTK_IM_MODULE=fcitx QT_IM_MODULE=fcitx XMODIFIERS=@im=fcitx ${pkgs.wechat}/bin/wechat
-  '';
+  home.packages = [
+    (pkgs.symlinkJoin {
+      name = "wechat-fcitx";
+      paths = [ pkgs.wechat ];
+      buildInputs = [ pkgs.makeWrapper ];
+
+      # https://zhuanlan.zhihu.com/p/2051087997395808628
+      postBuild = ''
+        wrapProgram $out/bin/wechat \
+          --unset WAYLAND_DISPLAY \
+          --set DISPLAY ":0" \
+          --set QT_QPA_PLATFORM "xcb" \
+          --set GTK_IM_MODULE "fcitx" \
+          --set QT_IM_MODULE "fcitx" \
+          --set XMODIFIERS "@im=fcitx"
+      '';
+    })
+  ];
 
   my.navi-cheats.wechat = ''
     # run wechat, a social communication app serving over 1 billion users, supporting free chat, HD voice and video calls, Moments, and mobile payments, making communication and life more convenient.
-    r wechat
+    wechat
   '';
 }
