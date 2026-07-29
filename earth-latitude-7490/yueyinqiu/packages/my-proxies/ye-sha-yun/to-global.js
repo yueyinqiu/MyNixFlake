b804e59594520ea7694c224e7aa61f285dba96da
yueyinqiu <yueyinqiu@outlook.com>
2026-07-29 21:30:18 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/my-proxies/ye-sha-yun/to-global.js
+++ earth-latitude-7490/yueyinqiu/packages/my-proxies/ye-sha-yun/to-global.js
@@ -1,6 +1,8 @@
 function main(config, log) {
     config["mode"] = "global";
-    config["proxy-groups"] = [config["proxy-groups"][0]];
+    config["proxy-groups"] = [
+        config["proxy-groups"][1]    // url test
+    ];
     config["rules"] = [];
     return config;
 }
