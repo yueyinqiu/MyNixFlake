c8399cfa56143fe3b6d1c75eb03f082352ef5958
yueyinqiu <yueyinqiu@outlook.com>
2026-07-29 12:05:27 +0800

    pack

--- earth-latitude-7490/yueyinqiu/packages/mihomo-cli/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/mihomo-cli/default.nix
@@ -1,18 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = [
-    (pkgs.rustPlatform.buildRustPackage {
-      pname = "mihomo-tui";
-      version = "0.4.5";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "potoo0";
-        repo = "mihomo-tui";
-        rev = "v0.4.5";
-        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-      };
-
-      cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    })
+    (pkgs.callPackage ./package.nix {})
   ];
 }
---
+++ earth-latitude-7490/yueyinqiu/packages/mihomo-cli/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "mihomo-tui";
+  version = "0.4.5";
+  __structuredAttrs = true;
+
+  src = fetchFromGitHub {
+    owner = "potoo0";
+    repo = "mihomo-tui";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-tNeH4HnpUL6UipDtSTfQLcT0ruwkUkho5+M6Mlj5E4c=";
+  };
+
+  cargoHash = "sha256-JOa5otMJE1GqpGnsIvY/O4ps0N3N1wpepESigY0+Dic=";
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "A simple TUI dashboard for monitoring and managing Mihomo via its REST API";
+    homepage = "https://github.com/potoo0/mihomo-tui";
+    changelog = "https://github.com/potoo0/mihomo-tui/blob/${finalAttrs.src.rev}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = [ ];
+    mainProgram = "mihomo-tui";
+  };
+})
