5981b5ac76be4d65810f68f7e8aebcb9c480dea3
yueyinqiu <yueyinqiu@outlook.com>
2026-07-30 15:37:36 +0800

    test

--- earth-latitude-7490/nixos/default.nix
+++ earth-latitude-7490/nixos/default.nix
@@ -122,30 +122,26 @@
     [Cameras]
     cameras/size = 2
 
-    [Cameras/1]
     cameras/1/type = output
     cameras/1/mode = mmap, userptr, rw
     cameras/1/description = Virtual Camera (Output)
     cameras/1/formats = 1
     cameras/1/videonr = 10
 
-    [Cameras/2]
     cameras/2/type = capture
     cameras/2/mode = mmap, rw
-    cameras/2/description = Integrated Camera (UVC)
+    cameras/2/description = Virtual Camera
     cameras/2/formats = 2
     cameras/2/videonr = 11
 
     [Formats]
     formats/size = 2
 
-    # 1. 给 output 推流端用的格式（akvcam 的 output 只支持 RGB24 / BGR24）
     formats/1/format = RGB24
     formats/1/width = 1280
     formats/1/height = 720
     formats/1/fps = 30
 
-    # 2. 伪装给腾讯会议看（capture 端）的格式，选通用性最好的 YUY2
     formats/2/format = YUY2
     formats/2/width = 1280
     formats/2/height = 720
