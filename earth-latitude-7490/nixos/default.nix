ab5e594fc695eb90a2572713cf9cfc49525cafaa
yueyinqiu <yueyinqiu@outlook.com>
2026-07-30 14:11:57 +0800

    f

--- earth-latitude-7490/nixos/default.nix
+++ earth-latitude-7490/nixos/default.nix
@@ -105,9 +105,12 @@
 
   services.flatpak.enable = true;
 
-  kernelModules = [ "v4l2loopback" ];
-  extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-  extraModprobeConfig = ''
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
     options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
   '';
 
