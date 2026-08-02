fea1497a74822e038f4ab2ae593ee4c0cf5ef66a
yueyinqiu <yueyinqiu@outlook.com>
2026-08-02 15:46:47 +0800

    f

--- earth-latitude-7490/yueyinqiu/packages/nb/default.nix
+++ earth-latitude-7490/yueyinqiu/packages/nb/default.nix
@@ -3,9 +3,21 @@
     nb
   ];
   my.navi-cheats.nb = ''
-      $ note: nb list --- --column 1
+      $ note: nb list --filenames --no-id --- my-bash-escape-std
 
       # edit a note in the currect nb notebook
       nb edit <note>
+      
+      # disable nb auto sync
+      nb set auto_sync 0
+      
+      # disable nb auto sync
+      nb set auto_sync 0
+      
+      # set nb sync url
+      nb remote set <url>
+
+      # sync the currect nb notebook
+      nb sync
   '';
 }
