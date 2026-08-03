7a0e9f183f0c020bf5e957c27f82b113111493ee
yueyinqiu <yueyinqiu@outlook.com>
2026-08-03 12:11:29 +0800

    f

--- earth-latitude-7490/nixos/easytier/default.nix
+++ earth-latitude-7490/nixos/easytier/default.nix
@@ -1,21 +1,21 @@
 { ... }: {
   services.easytier.enable = true;
   services.easytier.instances.s2429.configFile = "/etc/easytier/s2429.toml";
+  # To generate the configuration file, just run:
+  # sudo easytier-core -d --network-name xxx --network-secret xxx -e xxx
+  #
+  # Then:
+  # sudo mkdir -p /etc/easytier
+  # sudo nano /etc/easytier/s2429.toml
 
   services.easytier.instances.easytier-pro = {
+    configServer = "$CONFIG_SERVER";
     environmentFiles = [
-      # ET_CONFIG_SERVER=xxxxxxxx
+      # CONFIG_SERVER=xxxxxxxx
       "/etc/easytier/easytier-pro.env"
     ];
     extraArgs = [
       "--secure-mode=true"
     ];
   };
-
-  # To generate the configuration file, just run:
-  # sudo easytier-core -d --network-name xxx --network-secret xxx -e xxx
-  #
-  # Then:
-  # sudo mkdir -p /etc/easytier
-  # sudo nano /etc/easytier/default.toml
 }
